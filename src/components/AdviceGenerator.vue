76ec34bf28eae35d9c0453d533630e64463fa042
younes <younesbarraddev@gmail.com>
2022-04-23 21:52:16 +0000

    ✅ finished advice gen app

---
+++ src/components/AdviceGenerator.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="advice-generator">
+    <div class="advice-content">
+      <h1 class="advice-title">Advice #{{ advice.id }}</h1>
+      <p class="advice-text">“{{ advice.advice }}”</p>
+
+      <DividerPattern />
+    </div>
+    <IconDice @click="rollDice" />
+  </div>
+</template>
+
+<script>
+import IconDice from "./design/IconDice.vue";
+import DividerPattern from "./design/DividerPattern.vue";
+export default {
+  data() {
+    return {
+      advice: {},
+    };
+  },
+  created() {
+    this.rollDice();
+  },
+  components: {
+    IconDice,
+    DividerPattern,
+  },
+  methods: {
+    async rollDice() {
+      await fetch("https://api.adviceslip.com/advice")
+        .then((response) => response.json())
+        .then((data) => {
+          this.advice = data.slip;
+        });
+    },
+  },
+};
+</script>
+
+<style scoped>
+.advice-generator {
+  position: relative;
+  width: 700px;
+}
+
+.advice-content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 2rem;
+  background-color: #313a49;
+  border-radius: 0.5rem;
+  box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.1);
+}
+
+.advice-title {
+  color: #52ffa8;
+  font-size: 14px;
+  letter-spacing: 4px;
+  text-transform: uppercase;
+  margin-bottom: 15px;
+}
+
+.advice-text {
+  color: #cee3e9;
+  font-size: 28px;
+}
+
+.divider-pattern {
+  margin: 2rem auto;
+}
+
+.dice-icon {
+  position: absolute;
+  bottom: -30px;
+  left: 50%;
+  transform: translateX(-50%);
+}
+</style>
---
+++ src/components/design/IconDice.vue
@@ -0,0 +1,29 @@
+<template>
+  <span class="dice-icon">
+    <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M20 0H4a4.005 4.005 0 0 0-4 4v16a4.005 4.005 0 0 0 4 4h16a4.005 4.005 0 0 0 4-4V4a4.005 4.005 0 0 0-4-4ZM7.5 18a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Z"
+        fill="#1f2732"
+      />
+    </svg>
+  </span>
+</template>
+
+<style scoped>
+.dice-icon {
+  padding: 20px;
+  border-radius: 100%;
+  background-color: #52ffa8;
+  cursor: pointer;
+  transition: all 0.3s ease-in-out;
+}
+
+.dice-icon:hover {
+  box-shadow: 0 0 30px 5px #52ffa8;
+}
+
+.dice-icon svg {
+  display: inline-block;
+  vertical-align: middle;
+}
+</style>
